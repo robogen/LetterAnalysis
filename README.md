5b191c91a63808d9c33523ba33989aca52835cd0
Jerrod Dixon <jtdixon2015@gmail.com>
2018-06-07 03:58:14 -0500

    nltk addition

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Demo for letter frequency analysis
 
 Text for the Constitution of the United States of America gathered from the [National Archives](https://www.archives.gov/founding-docs/constitution-transcript)
+
+Books gathered from [Gutenburg](http://www.gutenberg.org)
---
+++ analysis_nltk.py
@@ -0,0 +1,14 @@
+from nltk import word_tokenize, Text
+from nltk.probability import FreqDist
+
+tokens = ""
+
+with open('constitution.txt', 'r') as con:
+    contents = con.read()
+    tokens = word_tokenize(contents)
+	
+processed = Text(tokens)
+fdist = FreqDist(processed)
+processed.collocations()
+print(fdist.most_common(50))
+fdist.plot(50)
